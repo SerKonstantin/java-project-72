b24eb8fab101db4962e783da6db97475bea062c0
serebrianskii <serebrianskiik@gmail.com>
2024-01-22 17:59:50 +0100

    Project initialization - first part

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-### Hexlet tests and linter status:
+# Site analyzer
+
 [![Actions Status](https://github.com/SerKonstantin/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SerKonstantin/java-project-72/actions)
+[![Actions Status](https://github.com/SerKonstantin/java-project-72/actions/workflows/build.yml/badge.svg)](https://github.com/SerKonstantin/java-project-72/actions)
---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.javalin:javalin:5.6.3")
+    implementation("org.slf4j:slf4j-simple:2.0.11")
+    implementation("io.javalin:javalin-rendering:5.6.3")
+    implementation("gg.jte:jte:3.1.6")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+    }
+}
