77f536e0d3df23bae51a39341460f7ea1f535253
serebrianskii <serebrianskiik@gmail.com>
2024-01-23 15:26:49 +0100

    Add explicit root path for html with TemplateEngine instance

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,12 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.databases.BaseRepository;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,20 +27,20 @@ public class App {
         return Integer.parseInt(port);
     }
 
-    public static String getDB() {
+    public static String getJdbcUrl() {
         return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
     }
 
     private static String readResourceFile(String filename) throws IOException {
-        var inputStream = App.class.getClassLoader().getResourceAsStream(filename);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+        try (var inputStream = App.class.getClassLoader().getResourceAsStream(filename);
+             var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             return reader.lines().collect(Collectors.joining("\n"));
         }
     }
 
     public static Javalin getApp() throws SQLException, IOException {
         var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(getDB());
+        hikariConfig.setJdbcUrl(getJdbcUrl());
 
         var dataSource = new HikariDataSource(hikariConfig);
         var resourceFileName = "schema.sql";
@@ -47,6 +51,7 @@ public class App {
 
         BaseRepository.dataSource = dataSource;
 
+        JavalinJte.init(createTemplateEngine());
         var app = Javalin.create(config -> config.plugins.enableDevLogging());
 
         app.get("/", ctx -> {
@@ -54,5 +59,11 @@ public class App {
         });
 
         return app;
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        return TemplateEngine.create(codeResolver, ContentType.Html);
     }
 }
